a9e56f0b4f9cb407f524dc7b36cddb67e8d7640e
Steven Soekha <steven.soekha@outlook.com>
2022-04-07 17:34:33 +0200

    Copy to clipboard

---
+++ README.md
@@ -0,0 +1,10 @@
+### Install
+Python3
+`pip3 install -r requirements.txt`
+
+Python2
+`pip install -r requirements.txt`
+
+### Add dependencies
+Generates a requirements.txt
+`pipreqs ./ `
--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-from files import get_week_days
-from extract import add_ingredients_to_list
+import pyperclip as pc
+from lib.files import get_week_days
+from lib.extract import add_ingredients_to_list
 
 files = get_week_days()
 data = dict()
 
 # Extract from singles
 for file in files.get('singles'):
-    print('*** Extracting file as single ***', file)
+    print('>> Extracting file as single ***', file)
     add_ingredients_to_list(file, data)
 
 # Extract from doubles
 for file in files.get('doubles'):
-    print('*** Extracting file as single ***', file)
+    print('>> Extracting file as single ***', file)
     add_ingredients_to_list(file, data)
     add_ingredients_to_list(file, data)
 
+shoppinglist = ''
+
 for key in data:
     item = data[key]
-    print(item['amount'], item['text'])
+    shoppinglist = shoppinglist + f'{item["amount"]} {item["text"]}' + '\n'
+
+pc.copy(shoppinglist)
+print('\n***\nShoppinglist copied to clipboard!\n***\n')
